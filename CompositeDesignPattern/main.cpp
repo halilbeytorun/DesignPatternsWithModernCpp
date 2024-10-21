803df180d3f48a38340bd8feb877e9c4b12d462e
halilbeytorun <beytorun25@gmail.com>
2024-10-21 19:36:35 +0300

    add Composite Design Pattern example

---
+++ CompositeDesignPattern/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class API
+{
+public:
+	API(string name) : name_(name) {}
+  virtual void draw() const = 0;
+  virtual ~API() = default;
+protected:
+	string name_;
+};
+
+class Dot : public API
+{
+public:
+	Dot(string name) : API(name) {
+		
+	}
+  void draw() const override
+  {
+    cout << "Actual dot class named: " << name_ << "\n";
+  }
+};
+
+class Window : public API
+{
+public:
+	Window(string name) : API(name) {}
+  std::vector<unique_ptr<API>> elems;
+  void add_element(unique_ptr<API>&& ele)
+  {
+    elems.push_back(std::move(ele));
+  }
+  void draw() const override
+  {
+    cout << name_ << " calling childs\n";
+    for(auto& ele : elems)
+    {
+      ele->draw();
+    }
+  }
+};
+
+int main()
+{
+  unique_ptr<API> dot1 = make_unique<Dot>("dot1");
+  unique_ptr<API> dot2 = make_unique<Dot>("dot2");
+  unique_ptr<API> dot3 = make_unique<Dot>("dot3");
+  unique_ptr<API> dot4 = make_unique<Dot>("dot3");
+
+  Window win1("win1");
+  win1.add_element(move(dot1));
+
+  unique_ptr<Window> win2 = make_unique<Window>("win2");
+  win2->add_element(move(dot2));
+  win2->add_element(move(dot3));
+  win2->add_element(move(dot4));
+  
+  win1.add_element(move(win2));
+  win1.draw();
+
+  return 0;
+}
